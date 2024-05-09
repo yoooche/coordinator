55164ba967c37b65eff631343d4c5ea0715f2da9
louis.y <louis.y.2.tcg@gmail.com>
2024-05-09 22:48:18 +0800

    init: register center

---
+++ coodinator-service/src/main/java/com/yangche/coodinatorservice/CoodinatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yangche.coodinatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoodinatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoodinatorServiceApplication.class, args);
+    }
+
+}
---
+++ coodinator-service/src/test/java/com/yangche/coodinatorservice/CoodinatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangche.coodinatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoodinatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
